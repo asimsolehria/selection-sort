e357ec4790d562f7cb99dc05a9ec10580fa739c7
Asim Habib <de.asimhabib@gmail.com>
2020-10-21 22:15:44 +0500

    Tested code found no bugs

--- src/selectionsort/main.java
+++ src/selectionsort/main.java
@@ -7,4 +7,17 @@ package selectionsort;
  */
 public class main {
     
+    public static void main(String[] args) {
+        
+        int [] array={8,2,6,9,4,3,5,7,1};
+        
+        SelectionSort sort=new SelectionSort();
+        
+        sort.sort(array);
+        
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
 }
