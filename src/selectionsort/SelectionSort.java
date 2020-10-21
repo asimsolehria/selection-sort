0a987fc91bfc4e390a505b3174fb86c4993d62be
Asim Habib <de.asimhabib@gmail.com>
2020-10-21 22:10:52 +0500

    wrote method of selection sort

--- src/selectionsort/SelectionSort.java
+++ src/selectionsort/SelectionSort.java
@@ -6,7 +6,24 @@ package selectionsort;
  * @author asim
  */
 public class SelectionSort {
-
     
+    public void sort(int[] array)
+    {
+        for (int i = 0; i < array.length-1; i++) 
+        {
+            int min=i;
+            
+            for (int j = i+1; j < array.length; j++) 
+            {
+                if(array[j]<array[min])
+                    min=j;
+            }
+            
+            int temp=array[i];
+            array[i]=array[min];
+            array[min]=temp;
+        }
+         
+    }
     
 }
